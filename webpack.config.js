0d92e51c40ef3fbfa2147f6b620e421694f65def
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-03-03 17:12:18 +0000

    Move app to client

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,9 @@ module.exports = {
   devServer: {
     stats: 'errors-only',
   },
-  entry: './app/index.js',
+  entry: './client/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'build', 'client'),
     filename: 'app.js'
   },
   resolve: {
@@ -62,7 +62,7 @@ module.exports = {
   plugins: [
     // Copy our app's index.html to the build folder.
     new CopyWebpackPlugin([
-      { from: './app/index.html', to: 'index.html' }
+      { from: './client/index.html', to: 'index.html' }
     ]),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
