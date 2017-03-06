c4036982df571a765ff5bce0fdd628e2bc707683
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-03-06 09:16:49 +0000

    Show IPFS configuration on boot if DEV

--- server/routes/ipfs.js
+++ server/routes/ipfs.js
@@ -12,4 +12,32 @@ router.post('/', (req, res, next) => {
   res.json({ content: req.body.data })
 })
 
+const showIpfsConfig = () => {
+  console.log('Connected to IPFS')
+  ipfs_.id().then((instance) => {
+    console.log('--- --- --- --- --- --- --- ---')
+    console.log('IPFS CONFIGURATION:')
+    console.log()
+
+    // Show addresses
+    console.log('ADDRESSES:')
+    for (const i in instance.addresses) {
+      let aCs = instance.addresses[i].split('/')
+      aCs = aCs.splice(0, aCs.length - 2)
+      console.log('\t' + aCs.join('/'))
+    }
+
+    // Protocols
+    console.log()
+    console.log('ID:\t\t\t' + instance.id)
+    console.log('AGENT VERSION:\t\t' + instance.agentVersion)
+    console.log('PROTOCOL VERSION:\t' + instance.protocolVersion)
+
+    console.log('\nIPFS READY!')
+    console.log('--- --- --- --- --- --- --- ---')
+  })
+}
+
+if (process.env.NODE_ENV === 'development') showIpfsConfig()
+
 export const ipfs = router
