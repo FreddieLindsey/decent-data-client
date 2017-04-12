8127f9a97ddd82fb9d34cf46aae4678d7c6404f9
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-04-12 14:53:47 +0100

    Update contract to implement mappings from path to hash

--- contracts/IPFSStorage.sol
+++ contracts/IPFSStorage.sol
@@ -2,23 +2,33 @@ pragma solidity ^0.4.8;
 
 contract IPFSStorage {
 
-  struct IPFSHash {
+  struct IpfsHash {
     address submitter;
-    bytes32 valuePart1;
-    bytes32 valuePart2;
+    bytes32 part1;
+    bytes32 part2;
   }
 
   uint storedData;
-  mapping (string => IPFSHash) IpfsContents;
+  mapping (string => IpfsHash) hashes;
 
-  function add(string path, bytes32 hash1, bytes32 hash2) {
-    /*IpfsContents[path] = IPFSHash(msg.sender, hash1, hash2);*/
+  function add(string path, bytes32 hash1, bytes32 hash2) returns (string stored) {
+    /*submitter = msg.sender;*/
+    /*part1 = hash1;*/
+    /*part2 = hash2;*/
+    /*storedData = 1;*/
+    hashes[path] = IpfsHash({
+      submitter: msg.sender, part1: hash1, part2: hash2
+    });
     storedData++;
+    return path;
   }
 
   function get(string path) constant returns (bytes32 hash1, bytes32 hash2) {
-    IPFSHash h = IpfsContents[path];
-    return (h.valuePart1, h.valuePart2);
+    IpfsHash h = hashes[path];
+    return (h.part1, h.part2);
+    /*if (msg.sender != submitter)
+      throw;*/
+    /*return part1;*/
   }
 
   function size() constant returns (uint size) {
