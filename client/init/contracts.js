2b83ab5c4241e7a1d6bec26f600815c3ba8a927a
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-04-27 16:46:25 +0100

    Remove SimpleStorage

--- client/init/contracts.js
+++ client/init/contracts.js
@@ -1,10 +1,8 @@
 import contract from 'truffle-contract'
 
-import SimpleStorage from '../../contracts/SimpleStorage.sol'
 import IPFSStorage from '../../contracts/IPFSStorage.sol'
 
 export const contractArtifacts = {
-  SimpleStorage,
   IPFSStorage
 }
 
