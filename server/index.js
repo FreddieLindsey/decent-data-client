2951b0018391e7681a4a18c1598f031b5b123c4c
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-04-27 14:18:32 +0100

    Allow payloads of upto 50MB

--- server/index.js
+++ server/index.js
@@ -17,8 +17,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Use bodyparser to get data from a POST
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 
 // Enable routes
 app.use('/', default_)
