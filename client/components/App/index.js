7cbece5920631dff649225a071faf6981545f002
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-04-30 00:37:17 +0100

    Clean up of App, IndexPage

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -10,16 +10,8 @@ import {
 import Authenticate from '../AuthenticatePage'
 import Index from '../IndexPage'
 
-import FileViewer from '../FileViewer'
-import FileMetadataList from '../FileMetadataList'
-import FileDropper from '../FileDropper'
-import SecureKeyLoader from '../SecureKeyLoader'
-
 import {
   ipfsStorageAddressGet,
-  ipfsStorageSizeGet,
-  ipfsStorageIndexGet,
-  fileRetrieve,
 } from '../../actions'
 
 const mapStateToProps = (state) => {
@@ -28,19 +20,13 @@ const mapStateToProps = (state) => {
   } = state.security
   const { current } = state.accounts
   return {
-    authenticated: (!!current && !!privateKey && !!publicKey),
-    accounts: state.accounts,
-    files: state.files.stored,
-    IPFSStorage: state.IPFSStorage
+    authenticated: (!!current && !!privateKey && !!publicKey)
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
     handleAddressGet: () => dispatch(ipfsStorageAddressGet()),
-    handleSizeGet: () => dispatch(ipfsStorageSizeGet()),
-    handleIndexGet: (i) => dispatch(ipfsStorageIndexGet(i)),
-    handleFileRetrieve: (path) => dispatch(fileRetrieve(path)),
   }
 }
 
@@ -49,45 +35,11 @@ class App extends Component {
   static displayName = 'App'
   static propTypes = {
     authenticated: PropTypes.bool.isRequired,
-    accounts: PropTypes.shape({
-      all: PropTypes.arrayOf(PropTypes.string),
-      current: PropTypes.string
-    }).isRequired,
-    files: PropTypes.object.isRequired,
-    IPFSStorage: PropTypes.shape({
-      address: PropTypes.string,
-      size: PropTypes.number
-    }).isRequired,
-
     handleAddressGet: PropTypes.func.isRequired,
-    handleSizeGet: PropTypes.func.isRequired,
-    handleIndexGet: PropTypes.func.isRequired,
-    handleFileRetrieve: PropTypes.func.isRequired,
   }
 
   componentDidMount () {
     this.props.handleAddressGet()
-    this.props.handleSizeGet()
-  }
-
-  getData () {
-    const {
-      IPFSStorage: {
-        size
-      },
-      files
-    } = this.props
-
-    if (size && size != 0 && Object.keys(files).length == 0)
-      for (let i = 0; i < size; i++)
-        this.props.handleIndexGet(i)
-
-    for (const k in files) {
-      let item = files[k]
-      if (item.index !== undefined && !item.retrieving && !item.retrieved) {
-        this.props.handleFileRetrieve(k)
-      }
-    }
   }
 
   render () {
--- client/components/IndexPage/index.js
+++ client/components/IndexPage/index.js
@@ -4,16 +4,21 @@ import { connect } from 'react-redux'
 import styles from './index.scss'
 
 import {
+  ipfsStorageSizeGet,
+  ipfsStorageIndexGet,
 } from '../../actions'
 
 const mapStateToProps = (state) => {
   return {
-    account: state.accounts.current
+    files: state.files.stored,
+    IPFSStorage: state.IPFSStorage,
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    handleSizeGet: () => dispatch(ipfsStorageSizeGet()),
+    handleIndexGet: (i) => dispatch(ipfsStorageIndexGet(i)),
   }
 }
 
@@ -21,18 +26,45 @@ class Index extends Component {
 
   static displayName = 'Index'
   static propTypes = {
-    account: PropTypes.string.isRequired
+    files: PropTypes.object.isRequired,
+    IPFSStorage: PropTypes.shape({
+      address: PropTypes.string,
+      size: PropTypes.number
+    }).isRequired,
+
+    handleSizeGet: PropTypes.func.isRequired,
+    handleIndexGet: PropTypes.func.isRequired,
+  }
+
+  componentDidMount () {
+    this.props.handleSizeGet()
+    this.getData()
+  }
+
+  getData () {
+    const {
+      IPFSStorage: {
+        size
+      },
+      files
+    } = this.props
+
+    if (size && size != 0 && Object.keys(files).length == 0)
+      for (let i = 0; i < size; i++)
+        this.props.handleIndexGet(i)
   }
 
   render () {
     const {
-      account
+      IPFSStorage: {
+        size
+      }
     } = this.props
 
     return (
       <div className={ styles.container } >
         <div className={ styles.main } >
-          { account }
+          Size of contract: { size }
         </div>
       </div>
     )
