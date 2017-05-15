a670947fa91bb6ef040309bc2fbb7919215f66e1
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-05-15 12:33:17 +0100

    Register accounts

--- infra/testrpc/demo.js
+++ infra/testrpc/demo.js
@@ -17,9 +17,7 @@ const ipfsStorage = contract(IPFSStorage)
 registry.setProvider(web3.currentProvider)
 ipfsStorage.setProvider(web3.currentProvider)
 
-import {
-  validateRSAPrivateKey
-} from '../../utils'
+import register_ from './demo/register'
 
 let accounts = {}
 require('./accounts.json').forEach((a) => {
@@ -40,24 +38,12 @@ console.log()
 for (const i in accounts)
   console.log(`${pad(i, 12)}:\t${accounts[i].address}`)
 
-// Register patient_1 and patient_2
-validateRSAPrivateKey(
-  fs.readFileSync(path.resolve(__dirname, 'accounts', 'patient_1.rsa')), (_, s, p) => {
-    const publicKeyPem = forge.pki.publicKeyToPem(p)
-    ipfs.add([ { path: 'publicKey.pem', content: publicKeyPem }], (err, res) => {
-      if (err) return
-      const hash = res[0].hash
-      ipfsStorage.new(hash.slice(0, 32), hash.slice(32, 64), {
-        from: accounts['patient_1'].address, gas: 3000000, gasPrice: 10000000
-      })
-      .then((i) => {
-        accounts['patient_1'].ipfsStorage = i.address
-        return registry.deployed()
-      })
-      .then((i) => {
-        return i.addStore(accounts['patient_1'].ipfsStorage, {
-          from: accounts['patient_1'].address, gas: 3000000, gasPrice: 10000000
-        })
-      })
-    })
-  })
+// DEMO SCRIPT
+
+register(['patient_1', 'patient_2', 'doctor'])
+
+// UTILITIES
+
+async function register (registers) {
+  return await Promise.all(registers.map((u) => register_(u, accounts)))
+}
