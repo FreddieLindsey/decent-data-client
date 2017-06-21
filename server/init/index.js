8be0d6cb0e79e9b314992ec6d10da933fc43501d
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-06-21 19:31:10 +0100

    Remove unnecessary

--- server/init/index.js
+++
@@ -1,5 +0,0 @@
-import { web3 } from './web3'
-import { contractArtifacts, initialise } from './contracts'
-
-export const contracts = initialise(contractArtifacts, web3)
-export { addresses } from './addresses'
--- server/init/web3.js
+++
@@ -1,3 +0,0 @@
-import Web3 from 'web3'
-
-export const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
