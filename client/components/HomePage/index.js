e3c241f332972ad5d054d9bc075adbebea5878b5
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-06-21 23:58:35 +0100

    Remove App Authenticated

---
+++ client/components/HomePage/index.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class HomePage extends Component {
+
+  static displayName = 'Home Page'
+
+  render () {
+    return (
+      <div>
+        Home Page
+      </div>
+    )
+  }
+
+}
