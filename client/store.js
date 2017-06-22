c7a2dc276cda74995426c152abbd9a0507e660f4
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-06-22 10:45:20 +0100

    Implement persisted store

--- client/store.js
+++ client/store.js
@@ -3,6 +3,10 @@ import { applyMiddleware, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
+// Persist store
+import localForage from 'localforage'
+import { persistStore, autoRehydrate } from 'redux-persist'
+
 import reducers from './reducers'
 import {
   accountsInit
@@ -14,7 +18,10 @@ let middleware = (process.env.NODE_ENV !== 'production')
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(reducers, {}, composeEnhancer(middleware))
+const store = createStore(reducers, undefined, composeEnhancer(middleware, autoRehydrate()))
+
+// Persist store to storage
+persistStore(store, { storage: localForage })
 
 if (module.hot) module.hot.accept('./reducers', () => {
   console.log('HOT RELOADING REDUCERS')
