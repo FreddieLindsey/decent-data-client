dcfab718035e6a1b75778722fa2e49a2a86d69f5
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-06-22 09:48:42 +0100

    Implement navigation bar

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react'
-import { connect } from 'react-redux'
 import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom'
 
 import UploadPage from '../components/UploadPage'
@@ -9,72 +8,34 @@ import SharedIndexPage from '../components/SharedIndexPage'
 import ViewBlobPage from '../components/ViewBlobPage'
 import ShareBlobPage from '../components/ShareAdminPage'
 
+import Nav from './Nav'
+
 import Home from '../views/Home'
 import NotFound from '../views/NotFound'
 
 import styles from './App.scss'
 
-import {
-  logout
-} from '../actions'
-
-const mapStateToProps = (state) => ({})
-const mapDispatchToProps = (dispatch) => ({
-  handleLogout: () => dispatch(logout())
-})
-
-class App extends Component {
+export default class App extends Component {
 
   static displayName = 'App (Authenticated)'
-  static propTypes = {
-    handleLogout: PropTypes.func.isRequired
-  }
 
   render () {
     return (
       <BrowserRouter >
         <div className={ styles.container } >
-          <div className={ 'row ' + styles.condense } >
-            <div className={ 'col-xs-3 ' + styles.noPad } >
-              <div className={ styles.nav } >
-                <NavLink
-                  to='/upload'
-                  className={ styles.navLink }
-                  activeClassName={ styles.navLinkSelected } >
-                  Upload
-                </NavLink>
-                <NavLink
-                  to='/personal'
-                  className={ styles.navLink }
-                  activeClassName={ styles.navLinkSelected } >
-                  Personal
-                </NavLink>
-                <NavLink
-                  to='/shared'
-                  className={ styles.navLink }
-                  activeClassName={ styles.navLinkSelected } >
-                  Shared
-                </NavLink>
-                <button
-                  className={ styles.logout }
-                  onClick={ () => this.props.handleLogout() } >
-                  Logout
-                </button>
-              </div>
-            </div>
-            <div className={ 'col-xs-9 ' + styles.noPad } >
-              <div className={ styles.content }>
-                <Switch>
-                  <Route exact path='/' component={ Home } />
-                  <Route exact path='/upload' component={ UploadPage } />
-                  <Route exact path='/personal' component={ PersonalIndexPage } />
-                  <Route exact path='/shared' component={ SharedIndexPage } />
-                  <Route path='/personal/share' component={ ShareBlobPage } />
-                  <Route path='/personal/view' component={ ViewBlobPage } />
-                  <Route path='/shared/view' component={ ViewBlobPage } />
-                  <Route component={ NotFound } />
-                </Switch>
-              </div>
+          <Nav />
+          <div className='container' >
+            <div className={ styles.content }>
+              <Switch>
+                <Route exact path='/' component={ Home } />
+                <Route exact path='/upload' component={ UploadPage } />
+                <Route exact path='/personal' component={ PersonalIndexPage } />
+                <Route exact path='/shared' component={ SharedIndexPage } />
+                <Route path='/personal/share' component={ ShareBlobPage } />
+                <Route path='/personal/view' component={ ViewBlobPage } />
+                <Route path='/shared/view' component={ ViewBlobPage } />
+                <Route component={ NotFound } />
+              </Switch>
             </div>
           </div>
         </div>
@@ -83,5 +44,3 @@ class App extends Component {
   }
 
 }
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
