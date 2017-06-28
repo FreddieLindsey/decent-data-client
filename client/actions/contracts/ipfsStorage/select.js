040122ed3bb8477a8ab242cadd3d1d14ad85fe8c
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-06-28 14:54:45 +0100

    Select a group

--- client/actions/contracts/ipfsStorage/select.js
+++ client/actions/contracts/ipfsStorage/select.js
@@ -9,7 +9,7 @@ export const IPFSSTORAGE_SELECT_PENDING = 'IPFSSTORAGE_SELECT_PENDING'
 export const IPFSSTORAGE_SELECT_SUCCESS = 'IPFSSTORAGE_SELECT_SUCCESS'
 export const IPFSSTORAGE_SELECT_ERROR = 'IPFSSTORAGE_SELECT_ERROR'
 
-export const ipfsStorageSelect = (address) => {
+export const ipfsStorageSelect = (address, group) => {
   return (dispatch, getState) => {
     dispatch(ipfsStorageSelectPending(address))
     if (getState().IPFSStorage.identities[address]) {
